9dafcd1194305eda3084773b3d5302d1ef8850ed
Michael Daye <mgdaye@gmail.com>
2017-11-19 21:20:07 -0500

    adding changes to the views

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const handlebars = require('express-handlebars');
+const hbars = require('express-handlebars');
 const db = require("./models");
 
 const app = express();
