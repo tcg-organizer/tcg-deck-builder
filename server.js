7f7760cd08576652efec9a0e9eb2ed62efebedda
Josh Pierce <jepierce1992@gmail.com>
2017-11-21 21:02:48 -0500

    added footer prototype

--- server.js
+++ server.js
@@ -7,25 +7,20 @@ const path = require("path");
 const app = express();
 var PORT = process.env.PORT || 8080;
 
-//bodyparser setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//handlebars setup
 app.engine("handlebars", hbars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-//html route
 var routes = require("./routes/routes");
 
-//html route in use
 app.use("/", routes);
 
-//connects to DB before connection to server
 db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
