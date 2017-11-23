d83ca4009c1b2add2877d3e42fc07f8623d893b2
Katie Lejman <misskatiemary@gmail.com>
2017-11-23 01:08:42 -0500

    pulled in Gene's model js files.Finally working with the most utd version will start on backend handlebar views after Thanksgiving or Friday

--- controller/routes.js
+++ controller/routes.js
@@ -1,11 +1,41 @@
+const express = require("express");
+const pokeRouter = express.Router();
 
-var express = require("express");
-var router = express.Router();
+// const pokemon = { pokemon: "sandshrew"};
 
-var pokemon = { pokemon: "sandshrew"};
 
-router.get("/", function (req, res) {
-    res.render("index", pokemon)
+//this half handles the handlebars pages
+pokeRouter.get("/", function (req, res) {
+    res.render("index");
 });
 
-module.exports = router;
+pokeRouter.get("/deckList", function(req, res) {
+    res.render("deckList", );
+});
+
+pokeRouter.get("/cardSearch", function(req, res) {
+    res.render("cardSearch", );
+});
+
+
+
+//this half will handle api requests
+
+//adding a new card to your deck
+pokeRouter.post("/api/",function(req, res) {
+
+});
+
+//updating a cards in your deck
+pokeRouter.put("/api/",function(req, res) {
+
+});
+
+//delete a card from your deck
+pokeRouter.delete("/api/",function(req, res) {
+
+});
+
+
+module.exports = pokeRouter;
+
