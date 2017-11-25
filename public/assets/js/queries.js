cbf4d64d9a3321ce60869e64c709fde5a394658b
jwilson99 <juliewilsongt@gmail.com>
2017-11-25 10:42:44 -0500

    added submit ID to button and search ID to input in .handlebars; added on-click event for submit button, grabbed value to query, nested queries in functions for reuse.

--- public/assets/js/queries.js
+++ public/assets/js/queries.js
@@ -1,30 +1,54 @@
 // uses pokemon tcg scraper npm
 var scraper = require('pokemon-tcg-scraper');
 
-// query for a list of cards including the query value
-// example within
+// declaration of variables for later use, pokemon for query value and cardURL from the query
+var pokemon;
+var cardURL;
 
-// var pokemon = $("#search").value().trim();
-// pokemon = pokemon.replace(" ","-");
-var pokemon = "blastoise-ex";
-var pokemonURL;
+// On click function to grab search value after page load
+$(function() {
+    $("#submit").on("click", function(event){
+        event.preventDefault();
 
-scraper.scrapeSearchPage("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?cardName=" + pokemon).then(function(card){
-    // returns an object with the following information: numPages, cards
-    console.log(JSON.stringify(card,null,4));
-    // we will show each card as an image and store the url of the card within the image
-    var cards = card.cards;
-    for (var i = 0; i < cards.length; i++){
-        // create a container showing image and maybe the id and append it to some existing container
-        var newContainer;
-    }
-    // when clicked, the stored url will be used to query for the specific card for display
+        var searchValue = $("#search").val().trim();
 
+        if (searchValue !== ""){
+
+            pokemon = searchValue;
+            // spaces are replaced with "-" to match query syntax
+            pokemon = pokemon.replace(" ","-");
+
+            //runs a query
+            initialQuery();
+        }
+    })
 });
 
-// query for a specific card based on a specific URL (can be received from the basic query above)
-// example within
-scraper.scrapeCard("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xyp/XY30/").then(function(card){
-    // returns an object with the following information: id, name, image, type, superType, hp, abilities, rules, color, weaknesses, resistances, retreatCost
-    console.log(JSON.stringify(card, null, 4));
-});
+var pokemonExample = "blastoise-ex";
+
+// query for a list of cards including matching the query value, pokemon
+function initialQuery(){
+    scraper.scrapeSearchPage("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?cardName=" + pokemon).then(function(card){
+
+        // returns an object with the following information: numPages, cards
+        console.log(JSON.stringify(card,null,4));
+
+        // we will show each card as an image and store the url of the card within the image
+
+        var cards = card.cards;
+        for (var i = 0; i < cards.length; i++){
+            // data is sent to cardSearch.handlebars for display
+            // each displayed card has a stored URL used for a second query when clicked
+        }
+
+    });
+}
+
+function singleCardQuery() {
+    // query for a specific card based on a specific URL (can be received from the basic query above)
+
+    scraper.scrapeCard(cardURL).then(function(card){
+        // returns an object with the following information: id, name, image, type, superType, hp, abilities, rules, color, weaknesses, resistances, retreatCost
+        console.log(JSON.stringify(card, null, 4));
+    });
+}
