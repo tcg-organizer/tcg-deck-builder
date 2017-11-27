2ee6030a4556907250142a3050ac79b06f411385
Michael Daye <mgdaye@gmail.com>
2017-11-26 21:49:52 -0500

    search bar returns json data from api route

--- controller/routes.js
+++ controller/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const pokeRouter = express.Router();
+const query = require('./queries');
+
+// uses pokemon tcg scraper npm
+const scraper = require('pokemon-tcg-scraper');
 
 // const pokemon = { pokemon: "sandshrew"};
-const cardData = require("../public/assets/js/queries");
+const cardData = [];
 
 //this half handles the handlebars pages
 pokeRouter.get("/", function (req, res) {
@@ -15,6 +19,56 @@ pokeRouter.get("/deckList", function (req, res) {
 
 pokeRouter.get("/cardSearch", function (req, res) {
     res.render("cardSearch", {cardData: cardData});
+});
+
+pokeRouter.post("/api/search/:pokemon?", function(req, res){
+    var pokeSearch = req.params.pokemon;
+
+    console.log(pokeSearch);
+
+    // example variables for use in testing
+    var pokemonExample = "blastoise";
+    var cardURLExample = "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/bw-series/bw7/31/";
+
+// query for a list of cards including matching the query value, pokemon
+    function initialQuery(pokemon) {
+        scraper.scrapeSearchPage("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?cardName=" + pokemon).then(function (data) {
+
+            // returns an object with the following information: numPages, cards
+            console.log(JSON.stringify(data, null, 4));
+
+            // we will show each card as an image and store the url of the card within the image
+
+            var cards = data.cards;
+            for (var i = 0; i < cards.length; i++) {
+                // data is sent to cardSearch.handlebars for display
+                // each displayed card has a stored URL used for a second query when clicked
+                var newCard = {
+                    url: cards[i].url,
+                    image: cards[i].image,
+                    id: cards[i].id,
+                    query: function () {
+                        singleCardQuery(this.url);
+                    }
+                };
+
+                cardData.push(newCard);
+            }
+        });
+    }
+
+    function singleCardQuery(cardURL) {
+        // query for a specific card based on a specific URL (can be received from the basic query above)
+
+        scraper.scrapeCard(cardURL).then(function (data) {
+            // returns an object with the following information: id, name, image, type, superType, hp, abilities, rules, color, weaknesses, resistances, retreatCost
+            console.log(JSON.stringify(data, null, 4));
+            // this will lead to a modal opening with displayed data from the query
+        });
+    }
+
+    initialQuery(pokeSearch);
+    res.render("cardSearch", {cardData: cardData})
 });
 
 
--- public/assets/js/queries.js
+++ public/assets/js/queries.js
@@ -1,132 +1,37 @@
-// uses pokemon tcg scraper npm
-const scraper = require('pokemon-tcg-scraper');
-
-// declaration of variables for later use
-var pokemon;
-var cardArray = module.exports = [
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xyp/XY30/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/XYP/XYP_EN_XY30.png",
-        "id": "xyp/XY30"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xyp/XY122/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/XYP/XYP_EN_XY122.png",
-        "id": "xyp/XY122"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xy1/29/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/XY1/XY1_EN_29.png",
-        "id": "xy1/29"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xy1/30/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/XY1/XY1_EN_30.png",
-        "id": "xy1/30"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xy1/142/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/XY1/XY1_EN_142.png",
-        "id": "xy1/142"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/ex-series/ex6/104/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/EX6/EX6_EN_104.png",
-        "id": "ex6/104"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/g1/17/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/G1/G1_EN_17.png",
-        "id": "g1/17"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/g1/18/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/G1/G1_EN_18.png",
-        "id": "g1/18"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/ex-series/ex14/2/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/EX14/EX14_EN_2.png",
-        "id": "ex14/2"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/ex-series/ex14/14/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/EX14/EX14_EN_14.png",
-        "id": "ex14/14"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xy12/21/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/XY12/XY12_EN_21.png",
-        "id": "xy12/21"
-    },
-    {
-        "url": "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xy12/22/",
-        "image": "https://assets.pokemon.com/assets/cms2/img/cards/web/XY12/XY12_EN_22.png",
-        "id": "xy12/22"
-    }
-];
-
 // On click function to grab search value after page load
-// $(function () {
-//     $("#submit").on("click", function (event) {
-//         event.preventDefault();
-//
-//         // empties cardArray from previous searches
-//         cardArray = [];
-//
-//         var searchValue = $("#search").val().trim();
-//
-//         if (searchValue !== "") {
-//
-//             pokemon = searchValue;
-//             // spaces are replaced with "-" to match query syntax
-//             pokemon = pokemon.replace(" ", "-");
-//
-//             //runs a query
-//             initialQuery(pokemon);
-//         }
-//     })
-// });
 
-// example variables for use in testing
-var pokemonExample = "blastoise";
-var cardURLExample = "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/bw-series/bw7/31/";
+$(function () {
 
-// query for a list of cards including matching the query value, pokemon
-function initialQuery(pokemon) {
-    scraper.scrapeSearchPage("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?cardName=" + pokemon).then(function (data) {
+    $("#submit").on("click", function (event) {
 
-        // returns an object with the following information: numPages, cards
-        console.log(JSON.stringify(data, null, 4));
+        if ($("#search").val().trim().length === 0 ) {
 
-        // we will show each card as an image and store the url of the card within the image
+            // Usually show some kind of error message here
 
-        var cards = data.cards;
-        for (var i = 0; i < cards.length; i++) {
-            // data is sent to cardSearch.handlebars for display
-            // each displayed card has a stored URL used for a second query when clicked
-            var newCard = {
-                url: cards[i].url,
-                image: cards[i].image,
-                id: cards[i].id,
-                query: function () {
-                    singleCardQuery(this.url);
-                }
-            };
-            cardArray.push(newCard);
+            // Prevent the form from submitting
+            event.preventDefault();
+        } else {
+
+        var searchValue = $("#search").val().trim();
+
+            pokemon = searchValue;
+            // spaces are replaced with "-" to match query syntax
+            pokemon = pokemon.replace(" ", "-");
+
+            console.log(pokemon);
+
+            //ajax call to send data to the server
+            $.ajax({
+                method: "POST",
+                url: "/api/search/"+ pokemon
+            }).done(function(data){
+                console.log(data);
+
+            });
+
+            cardArray = [];
         }
-    });
-}
 
-function singleCardQuery(cardURL) {
-    // query for a specific card based on a specific URL (can be received from the basic query above)
-
-    scraper.scrapeCard(cardURL).then(function (data) {
-        // returns an object with the following information: id, name, image, type, superType, hp, abilities, rules, color, weaknesses, resistances, retreatCost
-        console.log(JSON.stringify(data, null, 4));
-        // this will lead to a modal opening with displayed data from the query
-    });
-}
-
-console.log(initialQuery("blastoise"))
+    })
+});
 
