bab815532d405142e4d78901d0c3320a988a308d
Gene Williams <GeneWillb@Gmail.com>
2017-11-28 14:32:01 -0500

    set up for auth0

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const db = require("./models");
 const path = require("path");
 const dbRoutes = require('./routes/dbRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+//auth0
+const logger = require('morgan');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const flash = require('connect-flash');
+
+dotenv.load();
+
+const routes = require('./routes/index');
+const user = require('./routes/user');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -25,21 +39,110 @@ app.engine("handlebars", hbars({ defaultLayout: "main" }));
 //This will render handlebars files when res.render is called.
 app.set("view engine", "handlebars");
 
-/*
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
-
- */
-
 app.use('/', htmlRoutes);
 app.use('/db', dbRoutes);
+app.use('/', routes);
+app.use('/user', user);
 
 
+// This will configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL:
+        process.env.AUTH0_CALLBACK_URL || 'http://localhost:8080/callback'
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
+        // extraParams.id_token has the JSON Web Token
+        // profile has all the information from the user
+        return done(null, profile);
+    }
+);
 
-// const routes = require("./controller/routes");
-// app.use("/", routes);
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+
+//todo check to see if you can have more than one view engine
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(
+    session({
+        secret: 'shhhhhhhhh',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Handle auth failure error messages
+app.use(function(req, res, next) {
+    if (req && req.query && req.query.error) {
+        req.flash("error", req.query.error);
+    }
+    if (req && req.query && req.query.error_description) {
+        req.flash("error_description", req.query.error_description);
+    }
+    next();
+});
+
+// Check logged in
+app.use(function(req, res, next) {
+    res.locals.loggedIn = false;
+    if (req.session.passport && typeof req.session.passport.user !== 'undefined') {
+        res.locals.loggedIn = true;
+    }
+    next();
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 
 db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
