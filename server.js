17f3a9426ca9e55d6c78c345e6c0e97158123e76
Gene Williams <GeneWillb@Gmail.com>
2017-11-30 15:21:35 -0500

    env set up loging in still broken

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const strategy = new Auth0Strategy(
         clientID: process.env.AUTH0_CLIENT_ID,
         clientSecret: process.env.AUTH0_CLIENT_SECRET,
         callbackURL:
-            'http://localhost:8080/' || process.env.AUTH0_CALLBACK_URL
+            'http://localhost:8080' || process.env.AUTH0_CALLBACK_URL
     },
     function(accessToken, refreshToken, extraParams, profile, done) {
         // accessToken is the token to call Auth0 API (not needed in the most cases)
