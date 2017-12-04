57ac42374371a5106a8618b7c5665e4c08c177fe
Michael Daye <mgdaye@gmail.com>
2017-12-04 16:42:47 -0500

    moved the connection to the jawsdb server to the sequelize init

--- server.js
+++ server.js
@@ -76,19 +76,20 @@ if (app.get('env') === 'development') {
     });
 }
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-
-    console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
 
 db.sequelize.sync({}).then(function() {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+        if (err) throw err;
+
+        console.log('The solution is: ', rows[0].solution);
+    });
+
+    connection.end();
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
